7a5db98610f11e38d1b09a1dd501df3d21451787
MLucifer <153346543+MLucifer13@users.noreply.github.com>
2024-11-07 12:26:23 +0545

    revert back

--- agents/views.py
+++ agents/views.py
@@ -1,41 +1,40 @@
 import random
-
 from django.core.mail import send_mail
-from django.views import generic
+from django.shortcuts import render, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import reverse
+from django.views import generic
 from leads.models import Agent
 from .forms import AgentModelForm
 from .mixins import OrganisorAndLoginRequiredMixin
-
 
 class AgentListView(OrganisorAndLoginRequiredMixin, generic.ListView):
     template_name = "agents/agent_list.html"
     
     def get_queryset(self):
         organisation = self.request.user.userprofile
-        return Agent.objects.filter(organisation=organisation)
 
+        return Agent.objects.filter(organisation= organisation)
+    
 
 class AgentCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
     template_name = "agents/agent_create.html"
     form_class = AgentModelForm
-
+        
     def get_success_url(self):
         return reverse("agents:agent-list")
-
-    def form_valid(self, form):
+    
+    def form_valid(self,form):
         user = form.save(commit=False)
         user.is_agent = True
         user.is_organisor = False
-        user.set_password(f"{random.randint(0, 1000000)}")
+        user.set_password(f"{random.randint(0,100000)}")
         user.save()
         Agent.objects.create(
             user=user,
-            organisation=self.request.user.userprofile
+            organisation = self.request.user.userprofile
         )
         send_mail(
-            subject="You are invited to be an agent",
+            subject="you are invited to be an agent",
             message="You were added as an agent on DJCRM. Please come login to start working.",
             from_email="admin@test.com",
             recipient_list=[user.email]
@@ -45,32 +44,33 @@ class AgentCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
 
 class AgentDetailView(OrganisorAndLoginRequiredMixin, generic.DetailView):
     template_name = "agents/agent_detail.html"
-    context_object_name = "agent"
+    context_object_name= 'agent'
 
     def get_queryset(self):
         organisation = self.request.user.userprofile
-        return Agent.objects.filter(organisation=organisation)
 
+        return Agent.objects.filter(organisation= organisation)
 
 class AgentUpdateView(OrganisorAndLoginRequiredMixin, generic.UpdateView):
     template_name = "agents/agent_update.html"
     form_class = AgentModelForm
 
+    def get_queryset(self):
+        return Agent.objects.all()
+        
     def get_success_url(self):
         return reverse("agents:agent-list")
-
-    def get_queryset(self):
-        organisation = self.request.user.userprofile
-        return Agent.objects.filter(organisation=organisation)
-
-
+    
+    
 class AgentDeleteView(OrganisorAndLoginRequiredMixin, generic.DeleteView):
     template_name = "agents/agent_delete.html"
-    context_object_name = "agent"
-
-    def get_success_url(self):
-        return reverse("agents:agent-list")
+    context_object_name= 'agent'
 
     def get_queryset(self):
         organisation = self.request.user.userprofile
-        return Agent.objects.filter(organisation=organisation)
+
+        return Agent.objects.filter(organisation= organisation)
+    
+    
+    def get_success_url(self):
+        return reverse("agents:agent-list")
--- leads/admin.py
+++ leads/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import User, Lead, Agent, UserProfile, Category
+from .models import User, Lead, Agent, UserProfile
 
-admin.site.register(Category)
 admin.site.register(Agent)
 admin.site.register(Lead)
 admin.site.register(User)
